1cad22ceedb1ea51be850dc4ee99b1080a381257
abhilash42 <abhilash.ub@gmail.com>
2017-08-30 13:24:54 +0530

    Estimate price with normal equation

--- machine-learning-ex1/ex1/computeCostMulti.m
+++ machine-learning-ex1/ex1/computeCostMulti.m
@@ -13,6 +13,9 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+pred = X*theta;
+sqrerr=(pred-y).^2;
+J=1/(2*m)*sum(sqrerr);
 
 
 
